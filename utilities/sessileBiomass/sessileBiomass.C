a469d0ee0e98678bf0118f81a0abf9b5ed966cf0
Rohit Suresh <rs670@duke.edu>
2025-07-21 21:30:23 -0400

    scaled time

--- utilities/sessileBiomass/sessileBiomass.C
+++ utilities/sessileBiomass/sessileBiomass.C
@@ -2,83 +2,68 @@
 #include <iostream>
 #include <fstream>
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
 int main(int argc, char *argv[])
 {
-
     std::ofstream bfile;
-    bfile.open ("sessileBiomass.dat");
-    
-    Foam::timeSelector::addOptions();  
-    Foam::argList args(argc,argv); 
+    bfile.open("Mtot_fraction.dat");
+
+    Foam::timeSelector::addOptions();
+    Foam::argList args(argc, argv);
 
     #include "createTime.H"
     Foam::instantList timeDirs = Foam::timeSelector::select0(runTime, args);
-    
+
     forAll(timeDirs, timei)
     {
-	
-	runTime.setTime(timeDirs[timei], timei);
-    
-	#include "createMesh.H"
+        runTime.setTime(timeDirs[timei], timei);
+        #include "createMesh.H"
 
-	Info << "Reading M" << endl;
-	volScalarField M
-	(
-	    IOobject
-	    (
-		"M",
-		runTime.timeName(),
-		mesh,
-		IOobject::MUST_READ,
-		IOobject::AUTO_WRITE
-	    ),
-	    mesh
-	);
+        Info << "Reading M" << endl;
+        volScalarField M
+        (
+            IOobject
+            (
+                "M",
+                runTime.timeName(),
+                mesh,
+                IOobject::MUST_READ,
+                IOobject::AUTO_WRITE
+            ),
+            mesh
+        );
 
-	// volume
+        const scalarField& Mfield = M.primitiveField();
+        const scalarField& V = mesh.V();
 
-	volScalarField cellVolume
-	(
-	    IOobject
-	    (
-		"cellVolume",
-		runTime.timeName(),
-		mesh,
-		IOobject::NO_READ,
-		IOobject::AUTO_WRITE
-	    ),
-	    mesh,
-	    dimensionedScalar("zero",dimVolume,0.0)
-	);
+        scalar biomass = 0.0;
+        scalar totalVolume = 0.0;
 
-	cellVolume.ref() = mesh.V();
+        forAll(Mfield, celli)
+        {
+            scalar mval = Mfield[celli];
+            scalar vol = V[celli];
 
-	dimensionedScalar domainVolume = gSum(cellVolume);
+            totalVolume += vol;
 
-	// Mtot(t) = integral of M over the domain (sessile biomass)
+            if (mval > SMALL)
+            {
+                biomass += mval * vol;
+            }
+        }
 
-	dimensionedScalar Mtot = dimensionedScalar("Mtot", M.dimensions()*dimVolume, gSum(M.primitiveField() * mesh.V()));
+        reduce(biomass, sumOp<scalar>());
+        reduce(totalVolume, sumOp<scalar>());
 
-	// dimensionedScalar Mtot = gSum(M.internalField() * mesh.V());
+        scalar MtotFraction = (totalVolume > VSMALL) ? (biomass / totalVolume) : 0.0;
 
-	Info << "Mtot = " << Mtot.value() << endl;
-	bfile << runTime.timeName() << " " << Mtot.value() << "\n";
-
-	Info << endl;
-	
+        Info << "Mtot (sessile biomass / domain volume) = " << MtotFraction << endl;
+        bfile << runTime.timeName() << " " << MtotFraction << "\n";
     }
-	
+
     Info << nl << "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
-	<< "  ClockTime = " << runTime.elapsedClockTime() << " s"
-	<< nl << endl;
+         << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+         << nl << endl;
 
     Info << "End\n" << endl;
-
     return 0;
-    
 }
-
-
-// ************************************************************************* //
